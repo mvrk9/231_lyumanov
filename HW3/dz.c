6ba48a92181c274d58b6438bf444b72375cb4f83
mvrk9 <marklymanov@gmail.com>
2025-06-11 02:32:03 +0300

    Change code to the right one

--- HW3/dz.c
+++ HW3/dz.c
@@ -1,76 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <time.h>
+#include <string.h>
+#include <regex.h>
 
-int *arr;          
-int N;            
-int sum = 0;        
-pthread_mutex_t lock; 
+void replace_all(const char *pattern, const char *text, const char *replacement) {
+    regex_t regex;
+    regmatch_t match;
 
-
-typedef struct {
-    int start;
-    int end;
-} ThreadData;
-
-void *worker(void *arg) {
-    ThreadData *data = (ThreadData *)arg;
-    int partial_sum = 0;
-    for (int i = data->start; i < data->end; i++) {
-        partial_sum += arr[i];
+    if (regcomp(&regex, pattern, REG_EXTENDED)) {
+        fprintf(stderr, "Invalid regular expression\n");
+        return;
     }
 
-    pthread_mutex_lock(&lock);
-    sum += partial_sum;
-    pthread_mutex_unlock(&lock);
+    size_t result_size = 1024;
+    char *result = malloc(result_size);
+    if (!result) {
+        perror("malloc");
+        regfree(&regex);
+        return;
+    }
 
-    free(data);
-    return NULL;
+    size_t pos = 0;
+    const char *current = text;
+
+    while (!regexec(&regex, current, 1, &match, 0)) {
+        size_t before_len = match.rm_so;
+        size_t repl_len = strlen(replacement);
+
+
+        if (pos + before_len + repl_len >= result_size) {
+            result_size *= 2;
+            result = realloc(result, result_size);
+            if (!result) {
+                perror("realloc");
+                regfree(&regex);
+                return;
+            }
+        }
+
+
+        strncpy(result + pos, current, before_len);
+        pos += before_len;
+
+        strcpy(result + pos, replacement);
+        pos += repl_len;
+
+        current += match.rm_eo;
+    }
+
+    strcpy(result + pos, current);
+    printf("Result: %s\n", result);
+
+    free(result);
+    regfree(&regex);
 }
 
-int main() {
-    printf("Enter N (>=16): ");
-    scanf("%d", &N);
-    if (N < 16) {
-        printf("N must be at least 16.\n");
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <regex> <text> <replacement>\n", argv[0]);
         return 1;
     }
 
-    arr = (int *)malloc(N * sizeof(int));
-    srand(time(NULL));
-    for (int i = 0; i < N; i++) {
-        arr[i] = rand() % 100; 
-    }
-
-    printf("Generated array:\n");
-    for (int i = 0; i < N; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    pthread_t threads[4];
-    pthread_mutex_init(&lock, NULL);
-
-    int part_size = N / 4;
-    for (int i = 0; i < 4; i++) {
-        ThreadData *data = (ThreadData *)malloc(sizeof(ThreadData));
-        data->start = i * part_size;
-        if (i == 3)
-            data->end = N;  
-        else
-            data->end = (i + 1) * part_size;
-
-        pthread_create(&threads[i], NULL, worker, data);
-    }
-
-    for (int i = 0; i < 4; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    pthread_mutex_destroy(&lock);
-    free(arr);
-
-    printf("Total sum: %d\n", sum);
+    replace_all(argv[1], argv[2], argv[3]);
     return 0;
 }
